90bad9e182fa378a75fc5cda007dc9adb4a8c413
Harshit (rgjny) <159179258+rgjny@users.noreply.github.com>
2026-01-04 14:36:14 +0530

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,28 +8,26 @@ import rehypeAutolinkHeadings from 'rehype-autolink-headings'
 import expressiveCode from 'astro-expressive-code'
 import siteConfig from './src/site.config'
 import { pluginLineNumbers } from '@expressive-code/plugin-line-numbers'
-import remarkDescription from './src/plugins/remark-description' /* Add description to frontmatter */
-import remarkReadingTime from './src/plugins/remark-reading-time' /* Add reading time to frontmatter */
-import rehypeTitleFigure from './src/plugins/rehype-title-figure' /* Wraps titles in figures */
+import remarkDescription from './src/plugins/remark-description'
+import remarkReadingTime from './src/plugins/remark-reading-time'
+import rehypeTitleFigure from './src/plugins/rehype-title-figure'
 import { remarkGithubCard } from './src/plugins/remark-github-card'
-import { fromHtmlIsomorphic } from 'hast-util-from-html-isomorphic'
 import rehypeExternalLinks from 'rehype-external-links'
-import remarkDirective from 'remark-directive' /* Handle ::: directives as nodes */
+import remarkDirective from 'remark-directive'
 import rehypeUnwrapImages from 'rehype-unwrap-images'
-import { remarkAdmonitions } from './src/plugins/remark-admonitions' /* Add admonitions */
-import remarkCharacterDialogue from './src/plugins/remark-character-dialogue' /* Custom plugin to handle character admonitions */
-import remarkUnknownDirectives from './src/plugins/remark-unknown-directives' /* Custom plugin to handle unknown admonitions */
-import remarkMath from 'remark-math' /* for latex math support */
-import rehypeKatex from 'rehype-katex' /* again, for latex math support */
-import remarkGemoji from './src/plugins/remark-gemoji' /* for shortcode emoji support */
-import rehypePixelated from './src/plugins/rehype-pixelated' /* Custom plugin to handle pixelated images */
+import { remarkAdmonitions } from './src/plugins/remark-admonitions'
+import remarkCharacterDialogue from './src/plugins/remark-character-dialogue'
+import remarkUnknownDirectives from './src/plugins/remark-unknown-directives'
+import remarkMath from 'remark-math'
+import rehypeKatex from 'rehype-katex'
+import remarkGemoji from './src/plugins/remark-gemoji'
+import rehypePixelated from './src/plugins/rehype-pixelated'
 
-// https://astro.build/config
 export default defineConfig({
-  site: siteConfig.site,
-  base: '/',
+  site: 'https://rgjny.github.io',
   trailingSlash: siteConfig.trailingSlashes ? 'always' : 'never',
   prefetch: true,
+
   markdown: {
     remarkPlugins: [
       [remarkDescription, { maxChars: 200 }],
@@ -58,12 +56,15 @@ export default defineConfig({
       rehypeKatex,
     ],
   },
+
   image: {
     responsiveStyles: true,
   },
+
   vite: {
     plugins: [tailwindcss()],
   },
+
   integrations: [
     sitemap(),
     expressiveCode({
@@ -74,9 +75,10 @@ export default defineConfig({
         wrap: false,
       },
       plugins: [pluginLineNumbers()],
-    }), // Must come after expressive-code integration
+    }),
     mdx(),
   ],
+
   experimental: {
     contentIntellisense: true,
   },
